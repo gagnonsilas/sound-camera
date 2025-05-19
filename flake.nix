dccd85b7347f62d902aceb786349625bf6d492f3
gagnonsilas <gagnon.silas@gmail.com>
2025-05-19 17:13:41 -0400

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+
+        # canup = pkgs.writeShellScript
+        in
+      with builtins;
+      rec {
+
+        devShells.default = pkgs.mkShell {
+          name = "ev25";
+          packages = with pkgs; [
+            stm32cubemx
+            gcc-arm-embedded
+            bear
+            openocd
+            cmake
+            gcc
+            gdb
+            can-utils
+            python3Packages.python-can
+            (writeShellScriptBin "canup"  ''
+            sudo ip link set can0 type can bitrate 1000000
+            sudo ip link set up can0
+            '')
+            (writeShellScriptBin "vcanup"  ''
+            sudo ip link add dev vcan0 type vcan
+            sudo ip link set up vcan0
+            '')
+            (writeScriptBin "candump-fmt" ''
+            #!${pkgs.python3}/bin/python3
+            import sys
+
+            for line in sys.stdin:
+                print(" ".join((line[0:line.find("[") - 3] + "#").split()), end = "")
+                print(line[line.find("]") + 1: -1].replace(" ", ""))
+            '')
+
+            (writeShellScriptBin "sshcan" ''
+            vcanup
+            ssh $1 candump -ta $2 | candump-fmt | canplayer -v vcan0=$2
+            '')
+
+
+          ];
+        };
+      }
+    );
+}
